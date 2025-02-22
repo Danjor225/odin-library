0c0e6498ccdb18797a634cbaab8869be3106db30
Danjor225 <179250247+Danjor225@users.noreply.github.com>
2025-02-22 18:54:16 +0000

    Add new book btn & code

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
 const myLibrary = [];
+const libraryDisplay = document.querySelector('#libraryDisplay')
+const newBookDisplay = document.querySelector('#newBookDisplay')
 
 function Book(title, author, pages, read){
 
@@ -23,18 +25,29 @@ function displayBooks(){
     myLibrary.forEach(element => {
         
         bookDisplay = document.createElement('div')
-        document.body.appendChild(bookDisplay)
+        libraryDisplay.appendChild(bookDisplay)
         bookDisplay.textContent = element.displayBookContent()
 
     });
 
 }
+
+
+const addBookBtn = document.querySelector('#addBook')
+addBookBtn.addEventListener('click', (e)  => {
+    let titleInput = document.createElement('input')
+    newBookDisplay.appendChild(titleInput)
+    titleInput.placeholder = 'Title Of Book'
+})
+
+    
+
+
+
+
 addBookToLibrary('Lord of the Rings', 'J.R.R Tolkien', 268, 'No')
 addBookToLibrary('Jordans Life', 'Jordan', 1, 'Y')
 displayBooks();
 
-console.log(myLibrary)
 
 
-const consoleDisplay = document.querySelector('#console')
-consoleDisplay.textContent = myLibrary
