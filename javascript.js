9af8d257e232c75934ed998a8c87ba6b3b0032ca
Danjor225 <179250247+Danjor225@users.noreply.github.com>
2025-02-22 18:44:02 +0000

    Add display books & displayBookContent functions

--- javascript.js
+++ javascript.js
@@ -6,6 +6,9 @@ function Book(title, author, pages, read){
     this.author = author
     this.pages = pages
     this.read = read
+    this.displayBookContent = function(){
+        return `${this.title} by ${this.author} with ${pages} no of pages`
+    };
 
 }
 
@@ -15,13 +18,23 @@ function addBookToLibrary(title, author, pages, read){
     myLibrary.push(book)
 }
 
+function displayBooks(){
 
+    myLibrary.forEach(element => {
+        
+        bookDisplay = document.createElement('div')
+        document.body.appendChild(bookDisplay)
+        bookDisplay.textContent = element.displayBookContent()
+
+    });
+
+}
 addBookToLibrary('Lord of the Rings', 'J.R.R Tolkien', 268, 'No')
 addBookToLibrary('Jordans Life', 'Jordan', 1, 'Y')
+displayBooks();
 
 console.log(myLibrary)
 
 
 const consoleDisplay = document.querySelector('#console')
-
 consoleDisplay.textContent = myLibrary
